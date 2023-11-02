3847e2af9eceaea61abafd623db6396aa0132495
Juan Tellez <jmtp12poke@gmail.com>
2023-11-02 16:28:44 -0600

    project portafolio started

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+
+      </header>
+      <div className="title-section">
+        <h1>Juan Tellez</h1>
+        <h2>Software Engineer</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
