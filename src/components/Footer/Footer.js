11c88a1d8d49b7a568cf9612b1e0298bb9c021e2
Juan Tellez <jmtp12poke@gmail.com>
2023-11-18 13:19:33 -0600

    add properties for footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,23 @@ import '../../styles/Footer.css';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import { CONTACT_LINKS } from '../../utils/properties';
+
+
+const github_link = CONTACT_LINKS['github-link'];
+const linkedin_link = CONTACT_LINKS['linkedin-link'];
+const insta_link = CONTACT_LINKS['instagram-link'];
+
 
 const Footer = () => {
     return (
         <div className="footer">
-            <h1>Social Media</h1>
+            <h2 className='header-footer'>Social Media</h2>
 
             <div className='icons-section'>
-                <a href="https://github.com" target='_blank' rel="noreferrer"><GitHubIcon fontSize='large'>  </GitHubIcon> </a>
-                <a href="https://linkedin.com/feed/" target='_blank' rel="noreferrer"> <LinkedInIcon fontSize='large'> </LinkedInIcon> </a>
-                <a href="https://instagram.com" target='_blank' rel="noreferrer"> <InstagramIcon fontSize='large' >    </InstagramIcon> </a>
+                <a href={github_link} target='_blank' rel="noreferrer"><GitHubIcon fontSize='large'>  </GitHubIcon> </a>
+                <a href={linkedin_link} target='_blank' rel="noreferrer"> <LinkedInIcon fontSize='large'> </LinkedInIcon> </a>
+                <a href={insta_link} target='_blank' rel="noreferrer"> <InstagramIcon fontSize='large' >    </InstagramIcon> </a>
             </div>
             <div className='copyright-section'>
                 © {new Date().getFullYear} Copyright {' '}
---
+++ src/utils/properties.js
@@ -0,0 +1,7 @@
+
+
+export const CONTACT_LINKS = {
+    'github-link': 'https://github.com/juanM20',
+    'linkedin-link': 'https://www.linkedin.com/feed/',
+    'instagram-link': 'https://www.instagram.com/john_honor20/'
+}
